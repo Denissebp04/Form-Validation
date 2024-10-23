f67adfcf85e7e77863d79212b22e9bbe8bc18429
Denisse Belliard <153006924+Denissebp04@users.noreply.github.com>
2024-10-23 09:12:34 -0400

    testing

--- main.js
+++ main.js
@@ -1,21 +1,88 @@
-const registration = document.getElementsByClassName('registration')
-const regUser = document.getElementById('username')
-const email = document.getElementById('email').value
-const password = document.getElementById('password').value
-const submit = document.getElementById('submit')
+const registration = document.getElementById('registration')
+const regUser = document.getElementById('username').value
+const email = document.getElementById('email')
+const password = registration.elements['password'].value
+const pass = document.getElementById('registration-password')
+const confirmPass = document.getElementById('password-confirm').value
 
 
-registration.addEventListener('submit', function (e) {
-    // prevent the form from submitting
-    e.preventDefault();
-    if(!email.contains('example.com')){
-        alert('Invalid email')
+
+registration.addEventListener('submit', (e) => {
+    // e.preventDefault();
+
+    function validateEmail(){
+        let emailValue = email.value;
+        const atsign = emailValue.indexOf("@")
+        const dot = emailValue.lastIndexOf(".")
+
+        if(atsign < 1){
+            alert("Email must include @ symbol")
+            email.focus();
+            return false
+        }
+
+        if (dot - atsign < 2){
+            alert("Invalid format; '.' symbol must be at least 2 characters away from @ symbol")
+            email.focus()
+            return false
+        }
+        if(emailValue.includes("@example.com")){
+            alert("Email format can not contain '@example.com'")
+            email.focus()
+            return false;
+        }
+        return emailValue
+
+    }
+
+    const emailValidation = validateEmail()
+    if(emailValidation == false){
+        // e.returnValue = false
+        e.preventDefault()
         return false
     }
-    if(password.contains('password')){
-        alert('Invalid password')
-        return false
+
+    
+    function validatePassword(){
+        const passRegex = /^(?=.*[!@#$%^&*(),.?":{}|<>]).+$/
+        const regextest = passRegex.test(password)
+        console.log(password)
+        console.log("before "+ regextest)
+        if(!regextest){
+            console.log("after "+ regextest)
+            alert("Password must contain special character")
+            password.focus()
+            return false
+        }
+        if(password.includes("password")){
+            alert("Password can not contain the word 'password'")
+            pass.focus()
+            return false
+        }
+        if(password.includes(regUser)){
+            alert("Password can not contain username")
+            pass.focus()
+            return false
+
+        }
+        if(){
+            alert("Password can not contain username")
+            pass.focus()
+            return false
+        }
+
     }
+
+        return password
+
+
+    const passwordValidation = validatePassword()
+    if(passwordValidation == false){
+        e.preventDefault()
+        return false 
+    }
+    
+
 });
 
 
